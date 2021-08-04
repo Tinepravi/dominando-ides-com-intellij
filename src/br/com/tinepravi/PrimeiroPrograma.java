15986bddffa318af303de43dc998290f51e3c993
Tinepravi <tinepravi@live.com>
2021-08-03 23:21:59 -0300

    first commit

---
+++ src/br/com/tinepravi/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.tinepravi;
+import br.com.tinepravi.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Olá Mundo... "+(a+b));*/
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
